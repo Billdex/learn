0607cf1b655b1e8d41c6e757c29afe15bca216e5
Billdex <pc.luoxin@foxmail.com>
2021-02-26 16:51:27 +0800

    tag逻辑

--- go-gin-example/routers/router.go
+++ go-gin-example/routers/router.go
@@ -2,6 +2,7 @@ package routers
 
 import (
 	"github.com/gin-gonic/gin"
+	v1 "go-gin-example/routers/api/v1"
 
 	"go-gin-example/pkg/setting"
 )
@@ -15,11 +16,13 @@ func InitRouter() *gin.Engine {
 
 	gin.SetMode(setting.RunMode)
 
-	r.GET("/test", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "test",
-		})
-	})
+	apiv1 := r.Group("/api/v1")
+	{
+		apiv1.GET("/tags", v1.GetTags)
+		apiv1.POST("/tags", v1.AddTag)
+		apiv1.PUT("/tags/:id", v1.EditTag)
+		apiv1.DELETE("/tags/:id", v1.DeleteTag)
+	}
 
 	return r
 }
