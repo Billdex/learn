f36d60db89b779309395c01ba610d65a31fa8c11
Billdex <pc.luoxin@foxmail.com>
2021-03-03 17:24:09 +0800

    利用shutdown实现优雅退出

--- go-gin-example/main.go
+++ go-gin-example/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"go-gin-example/routers"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"go-gin-example/pkg/setting"
 )
@@ -18,6 +23,25 @@ func main() {
 		WriteTimeout:   setting.WriteTimeout,
 		MaxHeaderBytes: 1 << 20,
 	}
-
 	s.ListenAndServe()
+	go func() {
+		if err := s.ListenAndServe(); err != nil {
+			log.Printf("Listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal)
+	// 使用supervisor启动时，需要配置参数stopsignal=INT，使得chan能够正确捕获信号正确退出
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(setting.HTTPPort)*time.Second)
+	defer cancel()
+	if err := s.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+
+	log.Println("Server exiting")
 }
