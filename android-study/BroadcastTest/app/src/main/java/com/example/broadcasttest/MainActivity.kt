3defbd1287c9da20049af4c45ec5a1adfa46cd67
billdex <pc.luoxin@foxmail.com>
2021-03-21 17:33:44 +0800

    发送自定义广播

--- android-study/BroadcastTest/app/src/main/java/com/example/broadcasttest/MainActivity.kt
+++ android-study/BroadcastTest/app/src/main/java/com/example/broadcasttest/MainActivity.kt
@@ -7,6 +7,7 @@ import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,6 +20,11 @@ class MainActivity : AppCompatActivity() {
         intentFilter.addAction("android.intent.action.TIME_TICK")
         timeChangeReceiver = TimeChangeReceiver()
         registerReceiver(timeChangeReceiver, intentFilter)
+        button.setOnClickListener {
+            val intent = Intent("com.example.broadcasttest.MY_BROADCAST")
+            intent.setPackage(packageName)
+            sendBroadcast(intent)
+        }
     }
 
     override fun onDestroy() {
