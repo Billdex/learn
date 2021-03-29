10b05017a6569b4342830badf73aaa549f63c6fa
billdex <pc.luoxin@foxmail.com>
2021-03-30 01:33:10 +0800

    SharedPreference简单应用

--- android-study/BroadcastBestPractice/app/src/main/java/com/example/broadcastbestpractice/LoginActivity.kt
+++ android-study/BroadcastBestPractice/app/src/main/java/com/example/broadcastbestpractice/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.broadcastbestpractice
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -10,11 +11,17 @@ class LoginActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        loadAccount()
         login.setOnClickListener {
             val account = accountEdit.text.toString()
             val password = passwordEdit.text.toString()
             if (account == "admin" && password == "123456") {
                 val intent = Intent(this, MainActivity:: class.java)
+                if (rememberPass.isChecked) {
+                    rememberPassword(account, password)
+                } else {
+                    clearPassword()
+                }
                 startActivity(intent)
                 finish()
             } else {
@@ -22,4 +29,31 @@ class LoginActivity : BaseActivity() {
             }
         }
     }
+
+    private fun loadAccount() {
+        val prefs = getPreferences(Context.MODE_PRIVATE)
+        val isRemember = prefs.getBoolean("remember_password", false)
+        if (isRemember) {
+            val account = prefs.getString("account", "")
+            val password = prefs.getString("password", "")
+            accountEdit.setText(account)
+            passwordEdit.setText(password)
+            rememberPass.isChecked = true
+        }
+    }
+
+    private fun rememberPassword(account: String, password: String) {
+        val editor = getPreferences(Context.MODE_PRIVATE).edit()
+        editor.putBoolean("remember_password", true)
+        editor.putString("account", account)
+        editor.putString("password", password)
+        editor.apply()
+    }
+
+    private fun clearPassword() {
+        val editor = getPreferences(Context.MODE_PRIVATE).edit()
+        editor.clear()
+        editor.apply()
+    }
+
 }
