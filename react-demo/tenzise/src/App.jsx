7aedb596dcc7fe3f4bf35888bce97a3723fb55d9
billdex <pc.luoxin@foxmail.com>
2022-06-22 01:28:58 +0800

    tenzies游戏功能实现

--- react-demo/tenzise/src/App.jsx
+++ react-demo/tenzise/src/App.jsx
@@ -1,34 +1,78 @@
 import logo from './logo.svg'
 import './App.css'
-import React from "react";
+import React, {useEffect} from "react";
 import {nanoid} from "nanoid";
 import Die from "./components/die";
+import ReactConfetti from "react-confetti";
 
 function App() {
     const [dice, setDice] = React.useState(allNewDice)
+    const [step, setStep] = React.useState(0)
+    const [tenzies, setTenzies] = React.useState(false)
+    const [best, setBest] = React.useState(() => {
+        const bestStep = localStorage.getItem("best_step")
+        return bestStep ? parseInt(bestStep) : 999999
+    })
+
+    useEffect(() => {
+        const allHold = dice.every(die => die.isHeld)
+        const allNumEqual = dice.every(die => die.value === dice[0].value)
+        if (allHold && allNumEqual) {
+            setTenzies(true)
+        }
+    }, [dice])
+
+    useEffect(() => {
+        if (tenzies && step < best) {
+            localStorage.setItem("best_step", step.toString())
+            setBest(step)
+        }
+    }, [tenzies])
+
+    function generateDice() {
+        return {
+            value: Math.ceil(Math.random() * 6),
+            isHeld: false,
+            id: nanoid()
+        }
+    }
 
     function allNewDice() {
         const newDice = []
         for (let i = 0; i < 10; i++) {
-            newDice.push({
-                value: Math.ceil(Math.random() * 6),
-                isHeld: true,
-                id: nanoid()
-            })
+            newDice.push(generateDice())
         }
         return newDice
     }
 
+
     function rollDice() {
-        setDice(allNewDice)
+        setDice(dice => dice.map(die =>
+            die.isHeld ? die : generateDice()
+        ))
+        setStep(step => step + 1)
+    }
+
+    function HoldDie(id) {
+        setDice(dice => dice.map(die =>
+            die.id === id ? {
+                ...die,
+                isHeld: !die.isHeld,
+            } : die
+        ))
     }
 
     const diceElements = dice.map(dice => {
-        return <Die key={dice.id} value={dice.value} />
+        return <Die key={dice.id}
+                    value={dice.value}
+                    isHeld={dice.isHeld}
+                    holdCallback={() => HoldDie(dice.id)}
+        />
     })
 
     return (
         <div className="flex w-full h-screen bg-slate-700 justify-center items-center">
+            {tenzies && <ReactConfetti />}
             <div className="flex flex-col p-4 w-[28rem] h-[28rem] bg-gray-100 rounded-2xl place-content-around items-center">
                 <div className="text-center px-8">
                     <h1 className="font-medium text-4xl leading-normal">Tenzies</h1>
@@ -37,10 +81,18 @@ function App() {
                 <div className="grid grid-cols-5 gap-6">
                     {diceElements}
                 </div>
+                <div className="font-medium text-xl">
+                    Step: {step}
+                    {tenzies && step <= best && <span className="ml-8 font-mono text-orange-500">BEST!</span>}
+                </div>
                 <button
                     className="cursor-pointer font-sans w-36 h-14 bg-indigo-600 rounded-lg text-white font-medium text-2xl active:bg-indigo-700 active:shadow-inner"
-                    onClick={rollDice}
-                >Roll</button>
+                    onClick={tenzies ? () => {
+                        setStep(0)
+                        setTenzies(false)
+                        setDice(allNewDice())
+                    } : rollDice}
+                >{tenzies ? 'New Game' :'Roll'}</button>
             </div>
         </div>
     )
--- react-demo/tenzise/src/components/die.jsx
+++ react-demo/tenzise/src/components/die.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 
 export default function Die(props) {
-    return <div className="flex w-14 h-14 justify-center items-center bg-white rounded-md shadow-lg">
+    const dieStyle = props.isHeld ?
+        "flex w-14 h-14 justify-center items-center bg-green-400 rounded-md shadow-lg cursor-pointer" :
+        "flex w-14 h-14 justify-center items-center bg-white rounded-md shadow-lg cursor-pointer"
+    return <div className={dieStyle} onClick={props.holdCallback}>
         <span className="font-mono font-bold text-3xl">{props.value}</span>
     </div>
 }
